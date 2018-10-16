8c380f98c75f1ee684bd80fec1153293eb447537
Ashish Jha <amiableashish55@gmail.com>
2018-10-16 07:52:05 +0530

    Create create.py

---
+++ create.py
@@ -0,0 +1,5 @@
+import psycopg2
+connObj=psycopg2.connect("dbname=dq user=dq")
+cur=connObj.cursor()
+query="CREATE TABLE users (id integer PRIMARY KEY,  email text, name text, address text)"
+cur.execute(query)
